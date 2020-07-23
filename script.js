c975dad0f1829d135165e1f4cd3c2b886dd6e7ed
MattHeffNT <37504532+MattHeffNT@users.noreply.github.com>
2020-07-24 01:57:58 +0930

    -index.html
    bootstrap 4 submit button

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 document.querySelector('#submit').addEventListener("click",populate);
 
   // start of map shit 
-  var mapOptions = {
+  let mapOptions = {
     center: [-12.415932, 134.244268],
     zoom: 5
-  }
-  var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-  var map = new L.map('map', mapOptions);
+  };
+
+  const layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+  const map = new L.map('map', mapOptions);
   map.addLayer(layer);
 
-  var markerOptions = {
+  let markerOptions = {
     title: "MyLocation",
     clickable: true,
     draggable: true,
-  }
+  };
   
-  var marker = L.marker([-12.415932, 134.244268], markerOptions);
+  let marker = L.marker([-12.415932, 134.244268], markerOptions);
   marker.addTo(map);
 
     function populate(event) {
-
-
-
-
 
       let name = document.querySelector('#name').value;
       let poem = document.querySelector('#poem').value;
 
       // GeoJson textbox
-      let output = document.querySelector('#output')
+      let output = document.querySelector('#output');
 
       // marker lat/long
-      var LatiLongi = marker.getLatLng();
-      var Lat = LatiLongi.lat
-      var lng = LatiLongi.lng
+      let LatiLongi = marker.getLatLng();
+      let Lat = LatiLongi.lat;
+      let lng = LatiLongi.lng;
 
-      console.log(LatiLongi)
+      console.log(LatiLongi);
 
       output.innerHTML =
 
@@ -53,5 +53,4 @@ document.querySelector('#submit').addEventListener("click",populate);
         }
       }
    `
-
     }
